631f9df59b059ba8811495a3c4b93c316d8dbe42
therealchainman <toddey65@gmail.com>
2021-12-05 23:52:47 -0600

    day6

--- adventofcode/2021/day6/day61.cpp
+++ adventofcode/2021/day6/day61.cpp
@@ -1,27 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 80;
-long long days[9], pdays[9];
+long long lanternFish[9];
 int main() {
     freopen("inputs/input.txt", "r", stdin);
-    memset(days, 0, sizeof(days));
+    memset(lanternFish, 0, sizeof(lanternFish));
     string input, tmp;
     cin>>input;
     stringstream ss(input);
     while (getline(ss, tmp, ',')) {
-        pdays[stoll(tmp)]++;
+        lanternFish[stoll(tmp)]++;
     }
     for (int day = 0;day<N;day++) {
-        memset(days, 0, sizeof(days));
+        long long born = lanternFish[0];
         for (int i = 0;i<8;i++) {
-            days[i] += pdays[i+1];
+            lanternFish[i] = lanternFish[i+1];
         }
-        days[6] += pdays[0];
-        days[8] += pdays[0];
-        for (int i = 0;i<9;i++) {
-            pdays[i] = days[i];
-        }
+        lanternFish[6] += born;
+        lanternFish[8] = born;
     }
-    long long cnt = accumulate(pdays, pdays+9, 0LL);
+    long long cnt = accumulate(lanternFish, lanternFish+9, 0LL);
     cout<<cnt<<endl;
 }
