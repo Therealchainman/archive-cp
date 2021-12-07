c95be49887832a1e71dbedb11e1603bbadd0989a
therealchainman <toddey65@gmail.com>
2021-12-07 00:30:13 -0600

    day 7

--- adventofcode/2021/day7/day71.py
+++ adventofcode/2021/day7/day71.py
@@ -1,15 +1,7 @@
-from collections import namedtuple
+import numpy as np
+# sys.stdout = open('outputs/output.txt', 'w')
 with open("inputs/input.txt", "r") as f:
-    lines = list(map(int, f.read().split(',')))
-    mx = max(lines)
-    mn = min(lines)
-    lo = mn
-    hi = mx
-    res = 1000000000
-    print(mx)
-    for i in range(lo, hi+1):
-        s = 0
-        for crab in lines:
-            s += abs(crab - i)
-        res = min(res, s)
-    print(res)
+    positions = list(map(int, f.read().split(',')))
+    minFuel = int(abs(positions - np.median(positions)).sum())
+    print(minFuel)
+# sys.stdout.close()
--- adventofcode/2021/day7/day72.py
+++ adventofcode/2021/day7/day72.py
@@ -1,17 +1,5 @@
-from collections import namedtuple
+import numpy as np
 with open("inputs/input.txt", "r") as f:
-    lines = list(map(int, f.read().split(',')))
-    mx = max(lines)
-    mn = min(lines)
-    lo = mn
-    hi = mx
-    res = 100000000000000
-    print(mx)
-    for i in range(lo, hi+1):
-        s = 0
-        for crab in lines:
-            n = abs(crab-i)
-            k = n*(n+1)//2
-            s += k
-        res = min(res, s)
-    print(res)
+    positions = np.array(list(map(int, f.read().split(','))))
+    minFuel = int(sum(n*(n+1)/2 for n in abs(positions - int(np.mean(positions)))))
+    print(minFuel)
