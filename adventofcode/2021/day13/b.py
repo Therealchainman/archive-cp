2c4e177b02f107e8341d95c59135c92cf6dd5122
therealchainman <toddey65@gmail.com>
2021-12-13 00:13:07 -0600

    day 13 aoc 2021

---
+++ adventofcode/2021/day13/b.py
@@ -0,0 +1,77 @@
+from collections import defaultdict
+# sys.stdout = open('outputs/output.txt', 'w')
+
+class Solver:
+    def __init__(self):
+        self.data = set()
+        self.maxY = 0
+        self.maxX = 0
+        self.instructions = []
+    def dataLoader(self):
+        with open("inputs/input.txt", "r") as f:
+            while True:
+                line = f.readline().strip().split(',')
+                if len(line)<2:
+                    break
+                self.data.add((int(line[0]), int(line[1])))
+                self.maxX = max(self.maxX, int(line[0]))
+                self.maxY = max(self.maxY, int(line[1]))
+            while True:
+                line = f.readline().strip()
+                if not line:
+                    break
+                instruction = self.parse(line)
+                foldLine = int(line[line.find('=')+1:])
+                self.instructions.append((instruction, foldLine))
+    def parse(self, line):
+        return 1 if line.find('y')>=0 else 0
+    def foldUp(self,y):
+        for yi in range(y+1,self.maxY+1):
+            abovePivot = yi-y
+            for xi in range(self.maxX+1):
+                if (xi,yi) in self.data:
+                    self.data.remove((xi,yi))
+                    nr = y-abovePivot
+                    self.data.add((xi,nr))
+        self.maxY = y
+    def foldRight(self, x):
+        for xi in range(x+1,self.maxX+1):
+            rightPivot = xi-x
+            # print(rightPivot)
+            for yi in range(self.maxY+1):
+                # print(rightPivot)
+                if (xi,yi) in self.data:
+                    # print(xi,yi)
+                    self.data.remove((xi,yi))
+                    nc = x-rightPivot
+                    self.data.add((nc,yi))
+                    # print(xi,nc)
+        self.maxX = x
+    
+    def run(self):
+        self.dataLoader()
+        for instruction, foldLine in self.instructions:
+            if instruction == 1:
+                self.foldUp(foldLine)
+            else:
+                self.foldRight(foldLine)
+        return self.data
+    def makeGrid(self):
+        self.maxX = 0
+        self.maxY = 0
+        for x, y in self.data:
+            self.maxX = max(self.maxX, x)
+            self.maxY = max(self.maxY, y)
+        grid = [['.' for x in range(self.maxX+1)] for y in range(self.maxY+1)]
+        for x, y in self.data:
+            grid[y][x] = '#'
+        sgrid = ""
+        for row in grid:
+            sgrid += ''.join(row) + '\n'
+        return sgrid
+
+if __name__ == '__main__':
+    s = Solver()
+    data = s.run()
+    print(s.makeGrid())
+    
