57793d1b89627549bb2689e2b93865feb4384351
therealchainman <toddey65@gmail.com>
2021-12-16 03:20:47 -0600

    add problem with top sort

--- adventofcode/2021/day16/a.py
+++ adventofcode/2021/day16/a.py
@@ -319,31 +319,3 @@ class Solver:
 if __name__ == '__main__':
     s = Solver(None)
     print(s.run())
-
-
-"""
-0 = 0000
-1 = 0001
-2 = 0010
-3 = 0011
-4 = 0100
-5 = 0101
-6 = 0110
-7 = 0111
-8 = 1000
-9 = 1001
-A = 1010
-B = 1011
-C = 1100
-D = 1101
-E = 1110
-F = 1111
-38006F45291200
-3 -> 0011
-8 -> 1000
-
-VVVTTT , 4 then it's literal value
-
-00111000000000000110111101000101001010010001001000000000
-VVVTTTILLLLLLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBBBBBB
-"""
