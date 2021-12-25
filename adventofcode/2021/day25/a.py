ac8d81e2c5fa1a183df333474b33e930471e3d35
therealchainman <toddey65@gmail.com>
2021-12-25 04:14:53 -0600

    completed day 25 aoc 2021

--- adventofcode/2021/day25/a.py
+++ adventofcode/2021/day25/a.py
@@ -6,27 +6,36 @@ class SeaCucumber:
         data = self.data_loader()
         steps, R, C = 0, len(data), len(data[0])
         while True:
+            steps += 1
             ndata = [line[:] for line in data]
+            found = False
             for i in range(R):
                 for j in range(C):
                     if ndata[i][j]=='>' and ndata[i][(j+1)%C]=='.':
-                        ndata[i][j]='.'
+                        ndata[i][j]='r'
                         ndata[i][(j+1)%C]='x'
+                        found = True
+            for i in range(R):
+                for j in range(C):
+                    if ndata[i][j]=='r':
+                        ndata[i][j]='.'
+                    if ndata[i][j]=='x':
+                        ndata[i][j]='>'
             for j in range(C):
                 for i in range(R):
                     if ndata[i][j]=='v' and ndata[(i+1)%R][j]=='.':
-                        ndata[i][j]='.'
+                        ndata[i][j]='r'
                         ndata[(i+1)%R][j]='y'
+                        found = True
             for i in range(R):
                 for j in range(C):
-                    if ndata[i][j]=='x':
-                        ndata[i][j]='>'
+                    if ndata[i][j]=='r':
+                        ndata[i][j]='.'
                     if ndata[i][j]=='y':
                         ndata[i][j]='v'
-            if all(x==y for x,y in zip(data,ndata)):
+            if not found:
                 break
-            steps += 1
             data = ndata
-        print(steps-1)
+        print(steps)
 if __name__ == '__main__':
     SeaCucumber().run()
---
+++ adventofcode/2021/day25/day25.md
@@ -0,0 +1,48 @@
+
+# Part 1
+
+Solution:  Brute force algorithm to move the sea cucumbers in the grid.  
+
+```py
+class SeaCucumber:
+    def data_loader(self):
+        with open("inputs/input.txt", "r") as f:
+            return [list(line) for line in f.read().split('\n')]
+    def run(self):
+        data = self.data_loader()
+        steps, R, C = 0, len(data), len(data[0])
+        while True:
+            steps += 1
+            ndata = [line[:] for line in data]
+            found = False
+            for i in range(R):
+                for j in range(C):
+                    if ndata[i][j]=='>' and ndata[i][(j+1)%C]=='.':
+                        ndata[i][j]='r'
+                        ndata[i][(j+1)%C]='x'
+                        found = True
+            for i in range(R):
+                for j in range(C):
+                    if ndata[i][j]=='r':
+                        ndata[i][j]='.'
+                    if ndata[i][j]=='x':
+                        ndata[i][j]='>'
+            for j in range(C):
+                for i in range(R):
+                    if ndata[i][j]=='v' and ndata[(i+1)%R][j]=='.':
+                        ndata[i][j]='r'
+                        ndata[(i+1)%R][j]='y'
+                        found = True
+            for i in range(R):
+                for j in range(C):
+                    if ndata[i][j]=='r':
+                        ndata[i][j]='.'
+                    if ndata[i][j]=='y':
+                        ndata[i][j]='v'
+            if not found:
+                break
+            data = ndata
+        print(steps)
+if __name__ == '__main__':
+    SeaCucumber().run()
+```
