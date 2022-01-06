385b5a3274e8383e0113af9107521ee32dcdb8db
therealchainman <toddey65@gmail.com>
2022-01-06 06:40:56 -0600

    2 color for bipartite graph checking

--- cses/graph_algorithms/building-teams.cpp
+++ cses/graph_algorithms/building-teams.cpp
@@ -1,10 +1,5 @@
-# Graph Algorithms
-
-## Building Teams
-
-### Solution: Check graph is bipartite with 2 colors
-
-```c++
+#include <bits/stdc++.h>
+using namespace std;
 vector<int> colors;
 vector<vector<int>> graph;
 const string IMP = "IMPOSSIBLE";
@@ -50,50 +45,4 @@ int main() {
     } else {
         cout << IMP << endl;
     }
-
 }
-```
-
-## School Dance
-
-### Solution: Maximum Bipartite Matching with Kuhn's algorithm
-O(nm) time 
-
-```c++
-vector<bool> visited;
-vector<int> match;
-vector<vector<int>> graph;
-bool dfs(int u) {
-    if (visited[u]) return false;
-    visited[u] = true;
-    for (int& v : graph[u]) {
-        if (match[v] == -1 || dfs(match[v])) {
-            match[v] = u;
-            return true;
-        }
-    }
-    return false;
-}
-int main() {
-    int n, m, k, boy, girl;
-    } directed graph from boy nodes to girl nodes
-    for (int i = 0;i<k;i++) {
-        cin>>boy>>girl;
-        graph[boy].push_back(girl);
-    }
-    match.assign(m+1,-1);
-    for (int u=1;u<=n;u++) {
-        visited.assign(n+1, false);
-        dfs(u);
-    }
-    int cnt = accumulate(match.begin(), match.end(), 0, [](const auto& a, const auto& b) {
-        return a + (b!=-1);
-    });
-    cout<<cnt<<endl;
-    for (int i = 1;i<=m;i++) {
-        if (match[i]!=-1) {
-            printf("%d %d\n", match[i], i);
-        }
-    }
-}
-```
