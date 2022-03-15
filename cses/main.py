6def837c64cc32f2f55df1fb9f3634d4029cc239
Todd Chaney <LZ6RL7@NAM.corp.gm.com>
2022-03-15 04:57:49 -0500

    graph problems on cses

--- cses/main.py
+++ cses/main.py
@@ -1,31 +1,31 @@
-# Shortest Routes I
-
-## Solution: Classic Dijkstra Algorithm to find shortest distance to all nodes from a single source node. 
-
-```py
 from collections import defaultdict
 from math import inf
 from heapq import heappush, heappop
-
-def dijkstra(src):
+import io,os
+input = io.BytesIO(os.read(0,os.fstat(0).st_size)).readline
+ 
+def dijkstra(src, k):
     dist = [inf]*(n+1)
+    count = [0]*(n+1)
+    ANS = []
     minheap = []
     heappush(minheap, (0, src))
     while minheap:
         distance, city = heappop(minheap)
-        if dist[city] < inf: continue
-        dist[city] = distance
+        if count[city] >= k: continue
+        if city == n:
+            ANS.append(distance)
+        count[city] += 1
         for ncity, nw in graph[city]:
             ndistance = distance + nw
-            if ndistance < dist[ncity]:
+            if count[ncity] < k:
                 heappush(minheap, (ndistance, ncity))
-    return " ".join(map(str, dist[1:]))
-
+    return " ".join(map(str, ANS))
+ 
 if __name__ == '__main__':
-    n, m = map(int, input().split())
+    n, m, k = map(int, input().split())
     graph = defaultdict(list)
     for _ in range(m):
         a, b, c = map(int, input().split())
         graph[a].append((b,c))
-    print(dijkstra(1))
-```
+    print(dijkstra(1, k))
