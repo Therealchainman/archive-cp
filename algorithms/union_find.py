76074c4a9cbe7d746b25fce10ae0e8e2e4298ba5
Todd Chaney <LZ6RL7@NAM.corp.gm.com>
2022-03-31 00:37:28 -0500

    add union find

---
+++ algorithms/union_find.py
@@ -0,0 +1,18 @@
+class UnionFind:
+    def __init__(self,n):
+        self.size = [0]*n
+        self.parent = list(range(n))
+    
+    def find(self,i):
+        if i==self.parent[i]:
+            return i
+        self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self,i,j):
+        i, j = self.find(i), self.find(j)
+        if i!=j:
+            if self.size[i] < self.size[j]:
+                i,j=j,i
+            self.parent[j] = i
+            self.size[i] += self.size[j]
