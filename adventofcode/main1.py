4452cfe9b48c52d026b50e0e26703f518dd040aa
therealchainman <toddey65@gmail.com>
2022-12-04 02:11:28 -0600

    aoc day 4

--- adventofcode/main1.py
+++ adventofcode/main1.py
@@ -1,24 +1,13 @@
-class Fabric:
-    def __init__(self, fabric):
-        self.id = fabric.split()[0]
-        self.x = int(fabric.split()[2].split(',')[0])
-        self.y = int(fabric.split()[2].split(',')[1][:-1])
-        self.width = int(fabric.split()[3].split('x')[0])
-        self.height = int(fabric.split()[3].split('x')[1])
-        self.x2 = self.x + self.width
-        self.y2 = self.y + self.height
-    def __repr__(self):
-        return f'id: {self.id} x: {self.x} y: {self.y} width: {self.width} height: {self.height}'
+
 
 def main():
     with open('input.txt', 'r') as f:
         data = f.read().splitlines()
-        visited = [[0]*1000 for _ in range(1000)]
         res = 0
-        for fabric in map(Fabric, data):
-            for x, y in product(range(fabric.x, fabric.x2), range(fabric.y, fabric.y2)):
-                res += (visited[x][y]==1)
-                visited[x][y] += 1
+        for elf1, elf2 in map(lambda x: x.split(','), data):
+            s1, e1 = map(int, elf1.split('-'))
+            s2, e2 = map(int, elf2.split('-'))
+            res += (s1 >= s2 and e1 <= e2) or (s2 >= s1 and e2 <= e1)
         return res
 if __name__ == "__main__":
     print(main())
--- adventofcode/main2.py
+++ adventofcode/main2.py
@@ -1,22 +1,12 @@
-from itertools import *
-class Fabric:
-    def __init__(self, fabric):
-        self.id = int(fabric.split()[0][1:])
-        self.x = int(fabric.split()[2].split(',')[0])
-        self.y = int(fabric.split()[2].split(',')[1][:-1])
-        self.width = int(fabric.split()[3].split('x')[0])
-        self.height = int(fabric.split()[3].split('x')[1])
-        self.x2 = self.x + self.width
-        self.y2 = self.y + self.height
-    def __repr__(self):
-        return f'id: {self.id} x: {self.x} y: {self.y} width: {self.width} height: {self.height}'
-
+class Assignment:
+    def __init__(self, sections: str):
+        elf1, elf2 = sections.split(',')
+        self.s1, self.e1 = map(int, elf1.split('-'))
+        self.s2, self.e2 = map(int, elf2.split('-'))
 def main():
     with open('input.txt', 'r') as f:
-        data = list(map(Fabric, f.read().splitlines()))
-        for f1 in data:
-            if all(max(0, min(f1.x2, f2.x2) - max(f1.x, f2.x))*max(0, min(f1.y2, f2.y2) - max(f1.y, f2.y)) == 0 for f2 in data if f1 != f2):
-                return f1.id
-        return -1
+        data = f.read().splitlines()
+        res = sum([1 for work in map(Assignment, data) if min(work.e1, work.e2) - max(work.s1, work.s2) >= 0])
+        return res
 if __name__ == "__main__":
     print(main())
