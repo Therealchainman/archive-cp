1029f0811e90a5765875f91a493e37e26ca33cd5
therealchainman <toddey65@gmail.com>
2022-12-04 23:48:30 -0600

    aoc day 5

--- adventofcode/main1.py
+++ adventofcode/main1.py
@@ -1,35 +1,12 @@
-from collections import *
-class Event:
-    def __init__(self, data: str):
-        index = data.find(']')
-        self.date = data[1:index]
-        self.status = data[index+2:]
-    def __repr__(self):
-        return f'date: {self.date}, status: {self.status}'
 def main():
     with open('input.txt', 'r') as f:
-        data = sorted(list(map(Event, f.read().splitlines())), key = lambda event: event.date)
-        guards = defaultdict(lambda: [0]*60)
-        guard = None
-        # O(len(data))
-        for event in data:
-            if 'Guard' in event.status:
-                guard = int(event.status.split()[1][1:])
-            elif event.status == 'falls asleep':
-                start = int(event.date.split()[1].split(':')[1])
-                guards[guard][start] += 1
+        data = f.read()
+        stack = []
+        for ch in data:
+            if stack and stack[-1].lower() == ch.lower() and stack[-1] != ch:
+                stack.pop()
             else:
-                end = int(event.date.split()[1].split(':')[1])
-                guards[guard][end] -= 1
-        for key, vals in guards.items():
-            cnt = 0
-            for i in range(len(vals)):
-                cnt += vals[i]
-                vals[i] = cnt
-            guards[key] = vals
-        sleepiest_guard = max(guards.keys(), key = lambda key: sum(guards[key]))
-        sleepiest_day = max(range(len(guards[sleepiest_guard])), key = lambda time: guards[sleepiest_guard][time])
-        # O(len(guards) * 60) ~ O(len(data))
-        return sleepiest_guard*sleepiest_day
+                stack.append(ch)
+        return len(stack)
 if __name__ == "__main__":
     print(main())
--- adventofcode/main2.py
+++ adventofcode/main2.py
@@ -1,45 +1,18 @@
-from collections import *
-import re
-
-class Event:
-    def __init__(self, data: str):
-        pattern = r"[^[]*\[([^]]*)\]"
-        m = re.search(pattern, data)
-        self.date = m.group(1)
-        pattern = r"#[0-9]*"
-        m = re.search(pattern, data)
-        self.id = int(m.group(0)[1:]) if m else None
-        if self.id:
-            self.is_asleep = None
-        elif "asleep" in data:
-            self.is_asleep = True
-        else:
-            self.is_asleep = False
-    def __repr__(self):
-        return f'date: {self.date}, status: {self.is_asleep}'
+import string
 def main():
     with open('input.txt', 'r') as f:
-        data = sorted(list(map(Event, f.read().splitlines())), key = lambda event: event.date)
-        guards = defaultdict(lambda: [0]*60)
-        guard = None
-        # O(len(data))
-        for event in data:
-            if event.id:
-                guard = event.id
-            elif event.is_asleep:
-                start = int(event.date.split()[1].split(':')[1])
-                guards[guard][start] += 1
-            else:
-                end = int(event.date.split()[1].split(':')[1])
-                guards[guard][end] -= 1
-        for key in guards.keys():
-            cnt = 0
-            for i in range(len(guards[key])):
-                cnt += guards[key][i]
-                guards[key][i] = cnt
-        sleepiest_guard = max(guards.keys(), key = lambda key: max(guards[key]))
-        sleepiest_day = max(range(len(guards[sleepiest_guard])), key = lambda time: guards[sleepiest_guard][time])
-        # O(len(guards) * 60) ~ O(len(data))
-        return sleepiest_guard*sleepiest_day
+        data = f.read()
+        best = len(data)
+        for rem_ch in string.ascii_lowercase:
+            stack = []
+            for ch in data:
+                if ch.lower() == rem_ch:
+                    continue
+                if stack and stack[-1].lower() == ch.lower() and stack[-1] != ch:
+                    stack.pop()
+                else:
+                    stack.append(ch)
+            best = min(best, len(stack))
+        return best
 if __name__ == "__main__":
     print(main())
