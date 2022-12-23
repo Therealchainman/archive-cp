5c6ec034b1bc6014d45f736a0884e7cb3f83ade9
therealchainman <toddey65@gmail.com>
2022-12-22 23:46:28 -0600

    aoc day 23

--- adventofcode/main1.py
+++ adventofcode/main1.py
@@ -1,54 +1,66 @@
+from collections import *
+from itertools import *
 import time
-
-def display(grid):
-    print("\n".join(["".join(row) for row in grid]))
 
 def main():
     with open('input.txt', 'r') as f:
         data = f.read().splitlines()
-        grid = [list(line) for line in data[:-2]]
-        R = len(grid)
-        empty, wall, void = '.', '#', ' '
-        instructions = data[-1]
-        right, down, left, up = 0, 1, 2, 3
-        headings = {right: (0, 1), down: (1, 0), left: (0, -1), up: (-1, 0)}
-        def move(steps, heading, r, c):
-            for _ in range(steps):
-                nr, nc = r + headings[heading][0], c + headings[heading][1]
-                nr %= R
-                nc %= C
-                if grid[nr][nc] == wall: break
-                while grid[nr][nc] == void:
-                    nr += headings[heading][0]
-                    nc += headings[heading][1]
-                    nr %= R
-                    nc %= C
-                if grid[nr][nc] == wall: break
-                r = nr
-                c = nc
-            return r, c
-        C = 0
-        for row in grid:
-            C = max(C, len(row))
-        for i in range(R):
-            n = len(grid[i])
-            grid[i].extend([void]*(C-n))
-        r, c = 0, grid[0].index(empty)
-        num = 0
-        heading = right
-        for x in instructions:
-            if x.isdigit():
-                num = num * 10 + int(x)
-            else:
-                r, c = move(num, heading, r, c)
-                num = 0
-                if x == 'R':
-                    heading = (heading + 1) % 4
-                elif x == 'L':
-                    heading = (heading - 1) % 4
-        r, c = move(num, heading, r, c)
-        return 1000*(r+1) + 4*(c+1) + heading
+        grid = [list(line) for line in data]
+        R, C = len(grid), len(grid[0])
+        occupied = set()
+        elf = '#'
+        for r, c in product(range(R), range(C)):
+            if grid[r][c] == elf:
+                occupied.add((r, c))
+        north, south, west, east = (-1, 0), (1, 0), (0, -1), (0, 1)
+        northwest, southwest, northeast, southeast = (-1, -1), (1, -1), (-1, 1), (1, 1)
+        proposals = [north, south, west, east]
+        rounds = 10
+        for round in range(1, rounds+1):
+            next_move = {}
+            counter = Counter()
+            for r, c in occupied:
+                for proposal in proposals:
+                    taken = False
+                    proposed = None
+                    if proposal == north:
+                        for dr, dc in [north, northwest, northeast]:
+                            nr, nc = r + dr, c + dc
+                            if (nr, nc) in occupied:
+                                taken = True
+                                break
+                    elif proposal == south:
+                        for dr, dc in [south, southwest, southeast]:
+                            nr, nc = r + dr, c + dc
+                            if (nr, nc) in occupied:
+                                taken = True
+                                break
+                    elif proposal == west:
+                        for dr, dc in [west, northwest, southwest]:
+                            nr, nc = r + dr, c + dc
+                            if (nr, nc) in occupied:
+                                taken = True
+                                break
+                    elif proposal == east:
+                        for dr, dc in [east, northeast, southeast]:
+                            nr, nc = r + dr, c + dc
+                            if (nr, nc) in occupied:
+                                taken = True
+                                break
+                    if not taken: 
+                        proposed = proposal
+                        nr, nc = r + proposed[0], c + proposed[1]
+                        next_move[(r, c)] = (nr, nc)
+                        counter[(nr, nc)] += 1
+                        break
 
+            for (r, c), (nr, nc) in next_move.items():
+                if counter[(nr, nc)] == 1:
+                    occupied.discard((r, c))
+                    occupied.add((nr, nc))
+            proposals = proposals[1:] + proposals[:1]
+        minRow, maxRow, minCol, maxCol = min(r for r, c in occupied), max(r for r, c in occupied), min(c for r, c in occupied), max(c for r, c in occupied)
+        return sum(1 for r, c in product(range(minRow, maxRow+1), range(minCol, maxCol+1)) if (r, c) not in occupied)
 
 if __name__ == '__main__':
     start_time = time.perf_counter()
--- adventofcode/main2.py
+++ adventofcode/main2.py
@@ -1,103 +1,75 @@
 from collections import *
 from itertools import *
-import math
-
-class Cube:
-    def __init__(self, rows, cols, grid):
-        right, down, left, up = 0, 1, 2, 3
-        self.right, self.down, self.left, self.up = right, down, left, up
-        self.wrap = {1: {right: (2, right), down: (3, down), left: (4, right), up: (6, right)}, 2: {right: (5, left), down: (3, left), left: (1, left), up: (6, up)}, \
-            3: {right: (2, up), down: (5, down), left: (4, down), up: (1, up)}, 4: {right: (5, right), down: (6, down), left: (1, right), up: (3, right)}, \
-            5: {right: (2, left), down: (6, left), left: (4, left), up: (3, up)}, 6: {right: (5, up), down: (2, down), left: (1, down), up: (4, up)}}
-        self.headings = {right: (0, 1), down: (1, 0), left: (0, -1), up: (-1, 0)}
-        self.heading_map = ['right', 'down', 'left', 'up']
-        self.horizontal = (self.right, left)
-        self.vertical = (up, down)
-        self.empty, self.wall, self.void = '.', '#', ' '
-        self.R, self.C = rows, cols
-        self.grid = grid
-        self.build()
-
-    def build(self):
-        self.face = {}
-        cnt = 1
-        for r in range(len(self.grid)):
-            for c in range(len(self.grid[r])):
-                if self.grid[r][c] == self.void: continue
-                sr, sc = r, c
-                cu = self.construct(r, c)
-                self.face[cnt] = cu
-                cnt += 1
-    
-    def construct(self, starting_row, starting_col):
-        cube_face = [[' ']*self.C for _ in range(self.R)]
-        for r, c in product(range(starting_row, starting_row + self.R), range(starting_col, starting_col + self.C)):
-            cube_face[r-starting_row][c-starting_col] = self.grid[r][c]
-            self.grid[r][c] = self.void
-        return cube_face
-
-    def get_row_col(self, prev_heading, new_heading, row, col):
-        if new_heading in self.horizontal:
-            ncol = 0 if new_heading == self.right else self.C-1
-            nrow = row if prev_heading in self.horizontal else col
-        else:
-            nrow = 0 if new_heading == self.down else self.R-1
-            ncol = col if prev_heading in self.vertical else row
-        return nrow, ncol
-
-    def move(self, steps, heading, cur, r, c, cube):
-        for i in range(steps):
-            nr, nc = r + self.headings[heading][0], c + self.headings[heading][1]
-            ncur, nheading = cur, heading
-            if nr in (-1, self.R) or nc in (-1, self.C):
-                print('========Needs to transition to another face of the cube========')
-                print('cube, heading, row, col', cur, self.heading_map[heading], nr, nc)
-                ncur, nheading = self.wrap[cur][heading]
-                nr, nc = self.get_row_col(heading, nheading, r, c)
-                print('new cube, heading, row, col', ncur, self.heading_map[nheading], nr, nc)
-            if cube.face[ncur][nr][nc] == self.wall: break
-            r, c, cur, heading = nr, nc, ncur, nheading
-        print(f'number of movements: {i}')
-        return r, c, cur, heading
-
-            
-def display(grid):
-    print("\n".join(["".join(row) for row in grid]))
+import time
 
 def main():
     with open('input.txt', 'r') as f:
         data = f.read().splitlines()
-        grid = [list(line) for line in data[:-2]]
-        cube = Cube(50, 50, grid)
-        instructions = data[-1]
-        right, down, left, up = 0, 1, 2, 3
-        heading_map = ['right', 'down', 'left', 'up']
-        r = c = num = 0
-        heading = right
-        cur_cube = 1
-        cnt = 0
-        for x in instructions:
-            if x.isdigit():
-                num = num * 10 + int(x)
-            else:
-                print(f'move count: {cnt}')
-                print('starting position and facing direction and moves and cube', r, c, heading_map[heading], num, cur_cube)
-                r, c, cur_cube, heading = cube.move(num, heading, cur_cube, r, c, cube)
-                print('ending position and facing direction and cube', r, c, heading_map[heading], cur_cube)
-                print('--------------------------------------------------------------------------------------------------------------------------')
-                cnt += 1
-                # if cnt == 5: break
-                num = 0
-                if x == 'R':
-                    heading = (heading + 1) % 4
-                elif x == 'L':
-                    heading = (heading - 1) % 4
-        print('starting position and facing direction and moves and cube', r, c, heading_map[heading], num, cur_cube)
-        r, c, cur_cube, heading = cube.move(num, heading, cur_cube, r, c, cube)
-        print('ending position and facing direction and cube', r, c, heading_map[heading], cur_cube)
-        print('--------------------------------------------------------------------------------------------------------------------------')
-        return r, c, cur_cube, heading
-        
+        grid = [list(line) for line in data]
+        R, C = len(grid), len(grid[0])
+        occupied = set()
+        elf = '#'
+        for r, c in product(range(R), range(C)):
+            if grid[r][c] == elf:
+                occupied.add((r, c))
+        north, south, west, east = (-1, 0), (1, 0), (0, -1), (0, 1)
+        northwest, southwest, northeast, southeast = (-1, -1), (1, -1), (-1, 1), (1, 1)
+        proposals = [north, south, west, east]
+        rounds = 10000
+        for round in range(1, rounds+1):
+            next_move = {}
+            counter = Counter()
+            one_can_move = False
+            for r, c in occupied:
+                can_move = False
+                for dr, dc in [north, south, west, east, northwest, northeast, southwest, southeast]:
+                    nr, nc = r + dr, c + dc
+                    if (nr, nc) in occupied:
+                        can_move = True
+                if not can_move: continue
+                one_can_move = True
+                for proposal in proposals:
+                    taken = False
+                    proposed = None
+                    if proposal == north:
+                        for dr, dc in [north, northwest, northeast]:
+                            nr, nc = r + dr, c + dc
+                            if (nr, nc) in occupied:
+                                taken = True
+                                break
+                    elif proposal == south:
+                        for dr, dc in [south, southwest, southeast]:
+                            nr, nc = r + dr, c + dc
+                            if (nr, nc) in occupied:
+                                taken = True
+                                break
+                    elif proposal == west:
+                        for dr, dc in [west, northwest, southwest]:
+                            nr, nc = r + dr, c + dc
+                            if (nr, nc) in occupied:
+                                taken = True
+                                break
+                    elif proposal == east:
+                        for dr, dc in [east, northeast, southeast]:
+                            nr, nc = r + dr, c + dc
+                            if (nr, nc) in occupied:
+                                taken = True
+                                break
+                    if not taken: 
+                        proposed = proposal
+                        nr, nc = r + proposed[0], c + proposed[1]
+                        next_move[(r, c)] = (nr, nc)
+                        counter[(nr, nc)] += 1
+                        break
+            if not one_can_move:
+                return round
+            for (r, c), (nr, nc) in next_move.items():
+                if counter[(nr, nc)] == 1:
+                    occupied.discard((r, c))
+                    occupied.add((nr, nc))
+            proposals = proposals[1:] + proposals[:1]
+        return -1
+
 if __name__ == '__main__':
     start_time = time.perf_counter()
     print(main())
