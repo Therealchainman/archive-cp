5ee4498d37b890e00450907dac3c70d87f9f034e
therealchainman <toddey65@gmail.com>
2022-12-31 01:48:16 -0600

    cpp implementation of dinics algorithm for maximum flow + working on python mpm algo

---
+++ algorithms/maxflow.md
@@ -0,0 +1,112 @@
+# Maximum flow algorithms
+
+# Ford Fulkerson Algorithm
+
+## Dinics Algorithm for finding augmenting paths
+
+### Dinics implemented in cpp can it is fast on cses OJ
+
+```cpp
+#include <bits/stdc++.h>
+using namespace std;
+
+struct FlowEdge {
+    int src, dst;
+    long long cap, flow = 0;
+    void init(int u, int v, long long c) {
+        src = u;
+        dst = v;
+        cap = c;
+    }
+};
+
+struct Dinic {
+    const long long flow_inf = 1e18;
+    vector<FlowEdge> edges;
+    vector<vector<int>> adj_list;
+    int size, num_edges = 0;
+    int source, sink;
+    vector<int> level, ptr;
+    queue<int> q;
+
+    void init(int sz, int src, int snk) {
+        size = sz;
+        source = src;
+        sink = snk;
+        adj_list.resize(size);
+        level.resize(size);
+        ptr.resize(size);
+    }
+
+    void add_edge(int src, int dst, long long cap) {
+        FlowEdge fedge;
+        fedge.init(src, dst, cap);
+        edges.emplace_back(fedge);
+        FlowEdge residual_fedge;
+        residual_fedge.init(dst, src, 0);
+        edges.emplace_back(residual_fedge);
+        adj_list[src].push_back(num_edges);
+        adj_list[dst].push_back(num_edges + 1);
+        num_edges += 2;
+    }
+
+    bool bfs() {
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (int index: adj_list[node]) {
+                if (edges[index].cap - edges[index].flow < 1) continue;
+                if (level[edges[index].dst] != -1) continue;
+                level[edges[index].dst] = level[node] + 1;
+                q.push(edges[index].dst);
+            }
+        }
+        return level[sink] != -1;
+    }
+
+    long long dfs(int node, long long pushed) {
+        if (pushed == 0) return 0;
+        if (node == sink) return pushed;
+        for (int& cid = ptr[node]; cid < (int)adj_list[node].size(); cid++ ) {
+            int id = adj_list[node][cid];
+            int nei = edges[id].dst;
+            if (level[node] + 1 != level[nei] || edges[id].cap - edges[id].flow < 1) continue;
+            long long flow = dfs(nei, min(pushed, edges[id].cap - edges[id].flow));
+            if (flow == 0) continue;
+            edges[id].flow += flow;
+            edges[id^1].flow -= flow;
+            return flow;
+        }
+        return 0;
+    }
+
+    long long main() {
+        long long maxflow = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[source] = 0;
+            q.push(source);
+            if (!bfs()) break;
+            fill(ptr.begin(), ptr.end(), 0);
+            while (long long pushed = dfs(source, flow_inf)) {
+                maxflow += pushed;
+            }
+        }
+        return maxflow;
+    }
+};
+int main() {
+    Dinic dinic;
+    int n, m;
+    cin >> n >> m;
+    dinic.init(n, 0, n - 1);
+    for (int i = 0; i < m; i++) {
+        int src, dst, cap;
+        cin >> src >> dst >> cap;
+        dinic.add_edge(src - 1, dst - 1, cap);
+    }
+    long long res = dinic.main();
+    cout << res << endl;
+    return 0;
+}
+```
--- cses/main.py
+++ cses/main.py
@@ -45,21 +45,54 @@ class IOWrapper(IOBase):
 sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
 input = lambda: sys.stdin.readline().rstrip("\r\n")
 
-class MaxFlow:
+"""
+FordFulkeron algorithm for maximum flow problem 
+- pluggable augmenting path finding algorithms
+- residual graph
+- bottleneck capacity
+- flow edge class
+- depth first search
+- Edmonds-Karp algorithm
+- capacity scaling algorithm
+- dinics algorithm
+-- deadend elimination
+"""
+class FlowEdge:
+    def __init__(self, src: int, dst: int, cap: int):
+        self.src = src # source node
+        self.dst = dst # destination node
+        self.cap = cap
+
+    def __repr__(self):
+        return f'source node: {self.src}, destination node: {self.dst}, capacity: {self.cap} ======'
+
+class FordFulkersonMaxFlowV2:
+    """
+    Ford-Fulkerson algorithm 
+    - pluggable augmenting path finding algorithms
+    - residual graph
+    - bottleneck capacity
+    """
     def __init__(self, n: int, edges: List[Tuple[int, int, int]]):
         self.size = n
         self.edges = edges
 
     def build(self, n: int, edges: List[Tuple[int, int, int]]) -> None:
+        self.flowedges = []
         self.adj_list = {}
         self.delta = 0
         for u, v, cap in edges:
+            self.flowedges.append(FlowEdge(u, v, cap))
             if u not in self.adj_list:
-                self.adj_list[u] = Counter()
-            self.adj_list[u][v] += cap
+                self.adj_list[u] = []
+            self.adj_list[u].append(len(self.flowedges) - 1)
+            self.flowedges.append(FlowEdge(v, u, 0)) # residual edge
             if v not in self.adj_list:
-                self.adj_list[v] = Counter()
+                self.adj_list[v] = []
+            self.adj_list[v].append(len(self.flowedges) - 1)
             self.delta = max(self.delta, cap)
+        highest_bit_set = self.delta.bit_length() - 1
+        self.delta = 1 << highest_bit_set
 
     def main_dfs(self, source: int, sink: int) -> int:
         self.build(self.size, self.edges)
@@ -73,19 +106,22 @@ class MaxFlow:
         return maxflow
 
     def reset(self) -> None:
-        self.parents = [-1] * self.size
+        self.vis = [0] * self.size
+
+    def neighborhood(self, node: int) -> List[int]:
+        return (i for i in self.adj_list[node])
 
     def dfs(self, node: int, sink: int, flow: int) -> int:
         if node == sink:
             return flow
-        self.parents[node] = 1
-        cap = self.adj_list[node]
-        for nei, cap in cap.items():
-            if self.parents[nei] == -1 and cap > 0:
-                cur_flow = self.dfs(nei, sink, min(flow, cap))
+        self.vis[node] = 1
+        for index in self.neighborhood(node):
+            nei = self.flowedges[index]
+            if self.vis[nei.dst] == 0 and nei.cap > 0:
+                cur_flow = self.dfs(nei.dst, sink, min(flow, nei.cap))
                 if cur_flow > 0:
-                    self.adj_list[node][nei] -= cur_flow
-                    self.adj_list[nei][node] += cur_flow
+                    nei.cap -= cur_flow
+                    self.flowedges[index ^ 1].cap += cur_flow
                     return cur_flow
         return 0
     
@@ -94,6 +130,7 @@ class MaxFlow:
         maxflow = 0
         while True:
             self.reset()
+            self.parents = [-1] * len(self.flowedges)
             cur_flow = self.edmonds_karp(source, sink)
             if cur_flow == 0:
                 break
@@ -101,23 +138,24 @@ class MaxFlow:
         return maxflow
 
     def edmonds_karp(self, source: int, sink: int) -> int:
-        queue = deque([(source, math.inf)])
-        self.parents[source] = -2
+        queue = deque([(source, math.inf, -1)])
+        self.vis[source] = 1
         while queue:
-            node, flow = queue.popleft()
+            node, flow, prev_index = queue.popleft()
             if node == sink:
                 break
-            capacity = self.adj_list[node]
-            for nei, cap in capacity.items():
-                if self.parents[nei] == -1 and cap > 0:
-                    self.parents[nei] = node
-                    queue.append((nei, min(flow, cap)))
+            for index in self.neighborhood(node):
+                nei = self.flowedges[index]
+                if self.vis[nei.dst] == 0 and nei.cap > 0:
+                    self.vis[nei.dst] = 1
+                    self.parents[index] = prev_index
+                    queue.append((nei.dst, min(flow, nei.cap), index))
         if node == sink:
-            while node != source:
-                parent = self.parents[node]
-                self.adj_list[parent][node] -= flow
-                self.adj_list[node][parent] += flow # residual edge
-                node = parent
+            while prev_index != -1:
+                parent_index = self.parents[prev_index]
+                self.flowedges[prev_index].cap -= flow
+                self.flowedges[prev_index^1].cap += flow # residual edge
+                prev_index = parent_index
             return flow
         return 0
 
@@ -134,19 +172,60 @@ class MaxFlow:
             self.delta >>= 1
         return maxflow
 
-    def capacity_scaling(self, source: int, sink: int, flow: int) -> int:
-        if source == sink:
+    def capacity_scaling(self, node: int, sink: int, flow: int) -> int:
+        if node == sink:
             return flow
-        self.parents[source] = 1
-        capacity = self.adj_list[source]
-        for nei, cap in capacity.items():
-            if self.parents[nei] == -1 and cap > self.delta:
-                cur_flow = self.capacity_scaling(nei, sink, min(flow, cap))
+        self.vis[node] = 1
+        for index in self.neighborhood(node):
+            nei = self.flowedges[index]
+            if self.vis[nei.dst] == 0 and nei.cap >= self.delta:
+                cur_flow = self.capacity_scaling(nei.dst, sink, min(flow, nei.cap))
                 if cur_flow > 0:
-                    self.adj_list[source][nei] -= cur_flow
-                    self.adj_list[nei][source] += cur_flow
+                    nei.cap -= cur_flow
+                    self.flowedges[index ^ 1].cap += cur_flow
                     return cur_flow
         return 0
+
+    def dinics_bfs(self, source: int, sink: int) -> bool:
+        self.distances = [-1] * self.size
+        self.distances[source] = 0
+        queue = deque([source])
+        while queue:
+            node = queue.popleft()
+            for index in self.neighborhood(node):
+                nei = self.flowedges[index]
+                if self.distances[nei.dst] == -1 and nei.cap > 0:
+                    self.distances[nei.dst] = self.distances[node] + 1
+                    queue.append(nei.dst)
+        return self.distances[sink] != -1
+
+    def dinics_dfs(self, node: int, sink: int, flow: int) -> int:
+        if flow == 0: return 0
+        if node == sink: return flow
+        while self.ptr[node] < len(self.adj_list[node]):
+            index = self.adj_list[node][self.ptr[node]]
+            self.ptr[node] += 1
+            nei = self.flowedges[index]
+            if self.distances[nei.dst] == self.distances[node] + 1 and nei.cap > 0:
+                cur_flow = self.dinics_dfs(nei.dst, sink, min(flow, nei.cap))
+                if cur_flow > 0:
+                    nei.cap -= cur_flow
+                    self.flowedges[index ^ 1].cap += cur_flow
+                    return cur_flow
+        return 0
+
+    def main_dinics(self, source: int, sink: int) -> int:
+        self.build(self.size, self.edges)
+        maxflow = 0
+        while self.dinics_bfs(source, sink):
+            self.reset()
+            self.ptr = [0] * self.size # pointer to the next edge to be processed (optimizes for dead ends)
+            while True:
+                cur_flow = self.dinics_dfs(source, sink, math.inf)
+                if cur_flow == 0:
+                    break
+                maxflow += cur_flow
+        return maxflow
 
 def main():
     n, m = map(int, input().split())
@@ -157,7 +236,7 @@ def main():
         edges[i] = (u - 1, v - 1, cap)
         mx = max(mx, cap)
     source, sink = 0, n - 1
-    maxflow = MaxFlow(n, edges)
+    maxflow = FordFulkersonMaxFlowV2(n, edges)
     return maxflow.main_dfs(source, sink)
 
 if __name__ == '__main__':
